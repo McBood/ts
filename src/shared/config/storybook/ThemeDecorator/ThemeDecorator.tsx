11e23dac37c9a242a1cb1ad2cac5d7c8cc417162
McBood <mcboodvk@gmail.com>
2023-12-13 21:45:21 +0600

    some fixes

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,13 @@
-import { Story } from '@storybook/react';
-import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
+import React from "react";
+import { Story } from "@storybook/react";
+import { Theme, ThemeProvider } from "app/providers/ThemeProvider";
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+// eslint-disable-next-line react/display-name
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) =>
+  (
     <ThemeProvider initialTheme={theme}>
-        <div className={`app ${theme}`}>
-            <StoryComponent />
-        </div>
+      <div className={`app ${theme}`}>
+        <StoryComponent />
+      </div>
     </ThemeProvider>
-);
+  );
